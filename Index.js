c5a1928eb7fa7c761bc6533e00172208ad0d7545
mohsinrezashohag <mohsinrezashohag2@gmail.com>
2021-11-10 19:18:52 +0600

    hello

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const app = express();
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
-const app = express();
 require('dotenv').config()
 
 const port = process.env.PORT || 8000;
